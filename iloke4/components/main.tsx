74edcc41c507516531dd0cc62253d35623f8a006
dc4638 <dc4638@kakao.com>
2025-01-22 14:51:33 +0900

    아몰랑

---
+++ iloke4/components/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function main(props) {
+    return (
+        <View>
+            <Text>main</Text>
+        </View>
+    );
+}
+
+export default main;
