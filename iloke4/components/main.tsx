ff6eca758251f65fc015b028da3af9b5c6bfb001
DESKTOP-QRENH8C\user <01okok@naver.com>
2025-01-22 14:54:48 +0900

    셋팅

---
+++ iloke4/components/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function main(props) {
+    return (
+        <View>
+            <Text>메인나오나</Text>
+        </View>
+    );
+}
+
+export default main;
